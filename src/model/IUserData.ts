e3ed460cb125e67566ed08d2995f31f84d8a7653
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-05-09 16:49:33 -0300

    ajeitando model

--- src/model/IUserData.ts
+++ src/model/IUserData.ts
@@ -1,11 +1,11 @@
-import { followUserDTO } from "../types/DTO";
+import { followingUserDTO } from "../types/DTO";
 import { Post } from "./Post";
 import { User } from "./User";
 
 export interface IUserData {
   findUserByEmail(email: string): Promise<User>;
   insertUser(user: User): void;
-  insertNewFriend(follow: followUserDTO): void;
+  insertNewFriend(follow: followingUserDTO): void;
   findFollowId(friend_id: string): Promise<boolean>;
   removeFollow(friend_id: string): Promise<void>;
 
